f6aa492eef544b2b4c20957dc82d83f13d429841
Yihang Ho <hoyihang5@gmail.com>
2014-09-20 16:29:47 +0800

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Yihang Ho on 9/20/14.
+//  Copyright (c) 2014 Yihang Ho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
